9b8c1c20123b4634d07ed319232b642c861ddefe
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-29 02:09:13 +0800

    database connectetd

--- app.py
+++ app.py
@@ -3,8 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
-
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
+
+database = SQLAlchemy(app)
+ORM = Marshmallow(app)
+
+class Supplier(database.Model):
+    supp_id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(50), unique=True)
+    address = database.Column(database.String(100), unique=True)
+    category = database.Column(database.String(10))
+    
+    def __init__(self, name, address, category):
+    	self.name = name
+    	self.address = address
+    	self.category = category
+	
+class SupplierSchema(ORM.Schema):
+    class Meta:
+    	fields = ('supp_id', 'name', 'address', 'category')
+    	
+supplier_schema = SupplierSchema()
+suppliers_schema = SupplierSchema(many = True)
 
 @app.route('/', methods=['GET'])
 def get():
